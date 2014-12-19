751b9c8db094af7892985a7d20d1a7f65a3c08df
Wenfeng <wenfeng@venor.co>
2014-12-19 14:30:12 -0800

    more ipynb

--- py-examples/evalRPN.py
+++ py-examples/evalRPN.py
@@ -9,27 +9,30 @@ def evalRPN(tokens):
         #print tok, n
         if tok in op:
             if len(st)<2: return -1
-            a= int(st.pop())
-            b= int(st.pop())
-            #print a,b,tok
+            a= (st.pop())
+            b= (st.pop())
+            print a,b,tok
             if   tok == "+": st.append( a + b)
             elif tok == "-": st.append( b - a)
             elif tok == "*": st.append( a * b)
-            elif tok == "/": st.append( b / a)
+            elif tok == "/": st.append( int(b*1.0/a))
         else:
-            st.append(float(tok))
+            st.append(int(tok))
 
     print(st)
-    return st.pop()
+    return int(round(st.pop()))
 
 
-alist = ["2", "1", "+", "3", "*"]
-blist = ["4", "13", "5", "/", "+"]
+#alist = ["2", "1", "+", "3", "*"]
+#blist = ["4", "13", "5", "/", "+"]
+#print evalRPN(alist)
+#print evalRPN(blist)
+#alist = ["4", "3", "-"]
+#print evalRPN(alist)
+
+#alist = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+#print evalRPN(alist)
+
+alist = ["4","13","5","/","+"]
 print evalRPN(alist)
-print evalRPN(blist)
 
-alist = ["4", "3", "-"]
-print evalRPN(alist)
-
-alist = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
-print evalRPN(alist)
