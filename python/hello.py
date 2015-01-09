d33950bb020535dc6ebded10fb3d98a8c69c1da3
Wenfeng Wang <wenfeng@venor.co>
2015-01-08 16:55:27 -0800

    hello.py

---
+++ python/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def helloWorld(name="world"):
+    print "Hello, "+name+"!"
+    print "Welcome to the Brilent DataCamp!"
+
+if __name__ == "__main__":
+    helloWorld("world")
+
