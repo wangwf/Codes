885ae73b463e16f8b216a58cc83c2339d134f5ad
Wenfeng <wenfeng@venor.co>
2015-01-23 13:59:18 -0800

    new ipynb files

--- dataLogs/ex.py
+++ dataLogs/ex.py
@@ -2,13 +2,16 @@
 ## data Cleaning
 
 f  = open("learnDataSet.log", "r")
+import csv
+
 fo = open("output.txt","w")
+writer = csv.writer(fo)
 
 #line = f.readline()
 #words = line.split(":") # ;
 
 # Currently only extract a few features
-fo.write("response-size Runtime Content-Length request_method Path")
+fo.write("response-size,Runtime, Content-Length, request_method, Path\n")
 
 l=0
 for line in f:
@@ -17,7 +20,7 @@ for line in f:
         continue
     i=0
     record={}
-    alist =[0,0,0,'','','']
+    alist =[0,0,0,'','']
     while i<len(words):
         w =words[i]
         if w.startswith("error"):  # or regext ?
@@ -34,18 +37,25 @@ for line in f:
             res['Content-Length'] = int(words[i+1].split(",")[0])
             alist[2] =  int(words[i+1].split(",")[0])
         elif w.startswith("request_method"):
-            res['request_method']=w.split(";")[0].split("=")[1]
-            res['path']=w.split(";")[1].split("=")[1].split(",")[0]
-            alist[3] = w.split(";")[0].split("=")[1]
-            #alist[4] = w.split(";")[1].split("=")[1].split(",")[0]
+            ww = w.split(";")
+            res['request_method']=ww[0].split("=")[1]
+            alist[3]   = ww[0].split("=")[1]
+            if ww[1].split("=")[0].lstrip()=="path":
+                res['path']= ww[1].split("=")[1].split(",")[0]
+                alist[4]   = ww[1].split("=")[1].split(",")[0]
+                if l==0:
+                    print "== ",w, "   ", ww[1].split("=")[0]
+                    print words
+                l +=1
         i += 1
     if alist[0]>0:
-        for item in alist:
-            fo.write("%s "%item)
-        fo.write("\n")
-#    record['response']=res        
+        writer.writerow(alist)
+#        for item in alist:
+#            fo.write("%s "%item)
+#        fo.write("\n")
+#    record['response']=res
 #    fo.write(res)
-    
+
            # print res
             #fo.write(res)
 
