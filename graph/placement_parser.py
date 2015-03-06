055d12d918646b91c90c9bee480976ae041cb22f
Wenfeng <wenfeng.wang@yahoo.com>
2015-03-06 11:16:02 -0800

    placement

--- graph/placement_parser.py
+++ graph/placement_parser.py
@@ -11,8 +11,13 @@ for i in range(n):
     for j in range(m):
         i1,i2= (i+1)%n, (i-1+n)%n
         j1,j2= (j+1)%m, (j-1+m)%m
-#         print result[i][j], result[i1][j],
+#        print result[i][j], result[i1][j], result[i2][j],result[i][j1], result[i][j2]
 #        print result[i][j], result[i2][j]
 #        print result[i][j], result[i][j1]
 #        print result[i][j], result[i][j2]
-#        print i*n+j, i1*n+j, i2*n+j, i*n+j1, i*n+j2
+#        print 1+i*m+j, 1+i1*m+j, 1+i2*m+j, 1+i*m+j1, 1+i*m+j2
+        print 1+i*m+j, 1+i1*m+j, 1.0
+        print 1+i*m+j, 1+i2*m+j, 2.0
+        print 1+i*m+j, 1+i*m+j1, 3.0
+        print 1+i*m+j, 1+i*m+j2, 4.0
+
