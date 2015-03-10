062b6a3262eb8ee5db163366e582a0bf184acd42
Wenfeng <wenfeng.wang@yahoo.com>
2015-03-10 11:03:13 -0700

    update graphml

--- graph/placement_parser.py
+++ graph/placement_parser.py
@@ -7,8 +7,19 @@ result
 
 dirPath="/home/wenfeng/Venor/k-grid/hillclimb/calcserver/src/data-linkedin/"
 def readFile(filename):
+    import json
     with open(filename) as f:
-        return f.read()
+        attribution= f.read()
+        attribution=json.loads(attribution)
+        attribution.pop("apiStandardProfileRequest",None)
+        attribution.pop("id",None)
+        attribution.pop("pictureUrl",None)
+
+        res = attribution["firstName"]+attribution["lastName"] + "\n"
+        res += attribution['headline'].replace("&","-")
+        res +="\n"+ str(attribution["location"])
+
+        return res
     return ""
 
 
@@ -41,7 +52,7 @@ def outputgml(outfile="t.gml"):
 #             http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">"""
     s1 += "<graphml>\n"
 
-    s1 += """<key id="d0" for="node" attr.name="label" attr.type="string">  <default></default>   </key> \n"""
+    s1 += """<key id="d0" for="node" attr.name="label" attr.type="string">   </key> \n"""
     s1 += """<graph id="G" edgedefault="undirected">\n"""
 
 
@@ -77,7 +88,59 @@ def outputgml(outfile="t.gml"):
 
 
 outputgml("placement2.gml")
-outputgml("placement2.graphml")
+
+def outputgraphml(outfile="t.graphml"):
+    outF = open(outfile,"w")
+    s1="""<?xml version="1.0" encoding="UTF-8"?> \n"""
+
+#    s1 += """<graphml xmlns="http://graphml.graphdrawing.org/xmlns" 
+#             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+#             xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+#             http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">"""
+    s1 += "<graphml>\n"
+
+    s1 += """<key id="d0" for="node" attr.name="label" attr.type="string">   </key> \n"""
+    s1 += """<graph id="G" edgedefault="undirected">\n"""
+
+
+    outF.write(s1)
+    for i in range(n):
+        for j in range(m):
+            inode = 1+i*m+j
+            nodeName = result[i][j]
+            label= readFile(dirPath+result[i][j])
+
+#            outF.write('<node id="'+result[i][j]+'"/>\n')
+            outF.write('<node id="'+str(inode) +'">\n')
+            outF.write('  <data key="d0"> ' + str(label) + '</data>\n')
+            outF.write('</node>\n')
+
+    for i in range(n):
+        for j in range(m):
+            i1,i2= (i+1)%n, (i-1+n)%n
+            j1,j2= (j+1)%m, (j-1+m)%m
+#            source  = result[i][j]
+#            target1 = result[i1][j]
+#            target2 = result[i2][j]
+#            target3 = result[i][j1]
+#            target4 = result[i][j2]
+            source  = str(1 + i *m  + j)
+            target1 = str(1 + i1 *m + j)
+            target2 = str(1 + i2 *m + j)
+            target3 = str(1 + i *m  +j1)
+            target4 = str(1 + i *m  +j2)
+
+            outF.write('<edge source="'+source+'" target="'+target1+'"/>\n')
+            outF.write('<edge source="'+source+'" target="'+target2+'"/>\n')
+            outF.write('<edge source="'+source+'" target="'+target3+'"/>\n')
+            outF.write('<edge source="'+source+'" target="'+target4+'"/>\n')
+
+    outF.write("</graph>")
+    outF.write("</graphml>")
+    outF.close()
+
+
+outputgraphml("placement2.graphml")
 
 #
 #
